7f5a5da8b453387d06ed40f079e02e7dd548f723
ratoobi <69602231+ratoobi@users.noreply.github.com>
2022-02-04 20:44:50 -0300

    Split api from client

--- client/src/config.js
+++
@@ -1,4 +0,0 @@
-// const PORT = process.env.PORT || 5000;
-// const apiBaseUrl = window.location.origin + ":" + PORT + "/api";
-const apiBaseUrl = window.location.origin + "/api";
-export default apiBaseUrl;
