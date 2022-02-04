ea9b534dd36d4ea291c129764306fbc9293e5b09
ratoobi <69602231+ratoobi@users.noreply.github.com>
2022-02-04 20:38:00 -0300

    Split client from api

--- api/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const user = require("./user");
-const course = require("./course");
-
-router.use("/users", user);
-router.use("/courses", course);
-
-module.exports = router;
